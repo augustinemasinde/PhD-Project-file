fb8148692c3e2985d1a99d79e9fb95ce337fe675
augustine <augustinemasinde7@gmail.com>
2025-07-24 13:27:32 +0200

    data reading adjustment

--- my files/chik_foi_analysis.R
+++ my files/chik_foi_analysis.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(dplyr)
-chikdata <- read_excel("my files/chikungunya_data_Uganda.xlsx")
+library(here)
+chikdata <- read_excel(here("Data", "chikungunya_data_Uganda.xlsx"))
 chikdata <- chikdata %>%  select(UniqueKey, Year, Age_Yrs,IgM_CHIK)
 chikdata <- chikdata %>%
   mutate(
